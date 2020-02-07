9d14c01738f75be0a78788b135882e730d1f442c
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2020-02-07 21:12:28 +0200

    Revert "added writeAlgoDemo3"
    This reverts commit e06fb1be49f6dca022f18281b96827de912259fd.

--- js/app.js
+++ js/app.js
@@ -7,16 +7,9 @@ function writeAlgoDemo1() {
     document.write("<p>Площа прямокутника = " + side_a * side_b + "</p>");
 }
 
-function writeAlgoDemo2() {
+function writeAlgoDemo1() {
     let side_a = Math.random() * 10 + 5;
     let side_b = Math.random() * 10 + 5;
     document.write("<p>Side a = " + side_a + "</p><p>Side_b = " + side_b + "</p>");
     document.write("<p>Площа прямокутного трикутника = " + side_a * side_b / 2 + "</p>");
-}
-
-function writeAlgoDemo3() {
-    let radius = Math.random() * 10 + 5;
-    let square = Math.PI * radius * radius;
-    document.write("<p>Radius = " + radius + "</p>");
-    document.write("<p>Площа круга = " + square + "</p>");
 }
