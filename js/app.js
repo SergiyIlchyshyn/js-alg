e2ce5e376881f611e545d4af26be22c79f667cc3
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2020-02-07 21:06:21 +0200

    added algo for triangle

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
-function writeAlgoDemo() {
+function writeAlgoDemo1() {
     let side_a = Math.random() * 10 + 5;
     let side_b = Math.random() * 10 + 5;
-    document.write("Side a = " + side_a + "Side_b = " + side_b);
-    document.write("Об'єм куба = " + side_a * side_a * side_a);
-    document.write("Площа квадрата = " + side_a * side_a);
-    document.write("Площа прямокутника = " + side_a * side_b);
+    document.write("<p>Side a = " + side_a + "</p><p>Side_b = " + side_b + "</p>");
+    document.write("<p>Об'єм куба = " + side_a * side_a * side_a + "</p>");
+    document.write("<p>Площа квадрата = " + side_a * side_a + "</p>");
+    document.write("<p>Площа прямокутника = " + side_a * side_b + "</p>");
+}
+
+function writeAlgoDemo1() {
+    let side_a = Math.random() * 10 + 5;
+    let side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a = " + side_a + "</p><p>Side_b = " + side_b + "</p>");
+    document.write("<p>Площа прямокутного трикутника = " + side_a * side_b / 2 + "</p>");
 }
