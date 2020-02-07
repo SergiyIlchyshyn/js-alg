b74e86b494b472a92b315b35a535dc0c6dc1faea
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2020-02-07 20:53:04 +0200

    added javascript code

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function writeAlgoDemo() {
+    let side_a = Math.random() * 10 + 5;
+    let side_b = Math.random() * 10 + 5;
+    document.write("Side a = " + side_a + "Side_b = " + side_b);
+    document.write("Об'єм куба = " + side_a * side_a * side_a);
+    document.write("Площа квадрата = " + side_a * side_a);
+    document.write("Площа прямокутника = " + side_a * side_b);
+}
